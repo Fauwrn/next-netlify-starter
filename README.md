39e3eea1194394ef1edd235a6c12b34f23298bfb
Fauwrn <121767823+Fauwrn@users.noreply.github.com>
2024-02-04 04:01:38 +0000

    testing complete transfer

--- README.md
+++ README.md
@@ -1,90 +1,23 @@
-# Next + Netlify Starter
+# Obscurum-Sonus-Instrumentum-Tenebrarum-Attrahens
+Deliberatly obscure sound instrument - chaotic attractor
 
-[![Netlify Status](https://api.netlify.com/api/v1/badges/46648482-644c-4c80-bafb-872057e51b6b/deploy-status)](https://app.netlify.com/sites/next-dev-starter/deploys)
+This uses webgl and has been tested and optimised for use in safari. 
+If using chrome, make sure you have hardware acceleration turned on (you can test here: https://get.webgl.org/).
 
-This is a [Next.js](https://nextjs.org/) v12 project bootstrapped with [`create-next-app`](https://github.com/vercel/next.js/tree/canary/packages/create-next-app) and set up to be instantly deployed to [Netlify](https://url.netlify.com/SyTBPVamO)!
+Intended to be used at full window size.
 
-This project is a very minimal starter that includes 2 sample components, a global stylesheet, a `netlify.toml` for deployment, and a `jsconfig.json` for setting up absolute imports and aliases. With Netlify, you'll have access to features like Preview Mode, server-side rendering/incremental static regeneration via Netlify Functions, and internationalized routing on deploy automatically.
+Instructions are simple:
+1. Click anywhere to enable audio.
+2. Press space to randomise the text and audiofile used.
+3. Use the arrow keys to control the chaotic attractor. Up/Down effects speed. L/R effects attraction force.
+4. hover over words to trigger the samples.
 
-[![Deploy to Netlify](https://www.netlify.com/img/deploy/button.svg)](https://app.netlify.com/start/deploy?repository=https://github.com/netlify-templates/next-netlify-starter&utm_source=github&utm_medium=nextstarter-cs&utm_campaign=devex-cs)
+If at anytime you encounter an error, or the program ceases to work, refresh the page.
 
-(If you click this button, it will create a new repo for you that looks exactly like this one, and sets that repo up immediately for deployment on Netlify)
+Using your own audio files
+If you have your own audio files that you would like to use, add them to assets/audio/ and follow the naming convention.
+Note that the program is currenty set up to only use wav files. If you wish to use an alternative format (mp3,ogg) you must change the text in line 54 - '.wav' to the correct file type you wish to use. You must then update the file count variable to
+the number of audio files available.
 
-## Table of Contents:
-
-- [Getting Started](#getting-started)
-- [Installation options](#installation-options)
-- [Testing](#testing)
-  - [Included Default Testing](#included-default-testing)
-  - [Removing Renovate](#removing-renovate)
-  - [Removing Cypress](#removing-cypress)
-
-## Getting Started
-
-First, run the development server:
-
-```bash
-npm run dev
-# or
-yarn dev
-```
-
-Open [http://localhost:3000](http://localhost:3000) with your browser to see the result.
-
-You can start editing the page by modifying `pages/index.js`. The page auto-updates as you edit the file.
-
-### Installation options
-
-**Option one:** One-click deploy
-
-[![Deploy to Netlify](https://www.netlify.com/img/deploy/button.svg)](https://app.netlify.com/start/deploy?repository=https://github.com/netlify-templates/next-netlify-starter&utm_source=github&utm_medium=nextstarter-cs&utm_campaign=devex-cs)
-
-**Option two:** Manual clone
-
-1. Clone this repo: `git clone https://github.com/netlify-templates/next-netlify-starter.git`
-2. Navigate to the directory and run `npm install`
-3. Run `npm run dev`
-4. Make your changes
-5. Connect to [Netlify](https://url.netlify.com/Bk4UicocL) manually (the `netlify.toml` file is the one you'll need to make sure stays intact to make sure the export is done and pointed to the right stuff)
-
-## Testing
-
-### Included Default Testing
-
-We’ve included some tooling that helps us maintain these templates. This template currently uses:
-
-- [Renovate](https://www.mend.io/free-developer-tools/renovate/) - to regularly update our dependencies
-- [Cypress](https://www.cypress.io/) - to run tests against how the template runs in the browser
-- [Cypress Netlify Build Plugin](https://github.com/cypress-io/netlify-plugin-cypress) - to run our tests during our build process
-
-If your team is not interested in this tooling, you can remove them with ease!
-
-### Removing Renovate
-
-In order to keep our project up-to-date with dependencies we use a tool called [Renovate](https://github.com/marketplace/renovate). If you’re not interested in this tooling, delete the `renovate.json` file and commit that onto your main branch.
-
-### Removing Cypress
-
-For our testing, we use [Cypress](https://www.cypress.io/) for end-to-end testing. This makes sure that we can validate that our templates are rendering and displaying as we’d expect. By default, we have Cypress not generate deploy links if our tests don’t pass. If you’d like to keep Cypress and still generate the deploy links, go into your `netlify.toml` and delete the plugin configuration lines:
-
-```diff
-[[plugins]]
-  package = "netlify-plugin-cypress"
--  [plugins.inputs.postBuild]
--    enable = true
--
--  [plugins.inputs]
--    enable = false 
-```
-
-If you’d like to remove the `netlify-plugin-cypress` build plugin entirely, you’d need to delete the entire block above instead. And then make sure sure to remove the package from the dependencies using:
-
-```bash
-npm uninstall -D netlify-plugin-cypress
-```
-
-And lastly if you’d like to remove Cypress entirely, delete the entire `cypress` folder and the `cypress.config.ts` file. Then remove the dependency using:
-
-```bash
-npm uninstall -S cypress
-```
+Link to google drive folder w/ video:
+https://drive.google.com/drive/folders/1NBDt_evjv1_J7BcmmEpuVB8Cftsc0lS2?usp=sharing
---
+++ sketch.js
@@ -0,0 +1,290 @@
+///////////////////////////////////
+////OBSCURUS SONUS INSTRUMENTUM////
+////   TENEBRARUM ATTRAHENS    ////
+///////////////////////////////////
+////////////Variables//////////////
+let cam;                         // 
+////Chaotic Attractor Variables////
+let dim = 128;                   // 
+let scale = 25;                  //
+let t = 0.1;                     //
+let b = 0.19                     //
+let [xA,yA,zA] = [[],[],[]];     //
+let [xM,yM,zM] = [[],[],[]];     //
+//////////Text and Images//////////
+let wikiText = [0];              //
+let currentWikiText;             //
+let x = 0;                       //
+                                 //
+let splitString = [];            //
+let secRand = 0;                 //
+let cubeRotate=0 ;               //
+let vignetteColor = 0;           //
+                                 //
+let images = [0];                //
+let numImages = 10;              //
+/////////////File Loading//////////
+let input;                       //
+let audioFiles = [0];            //
+let audioFilesCount = 6;         //
+let audioFileSelect = 0;         //
+let soundFile;                   //
+let loopStart = 0.0;             //
+let loopDuration = 0.2;          //
+let distLevel=0;                 //
+                                 //
+///////////////////////////////////
+
+function preload() {
+  ///////////////////////////////////////////////   load fonts
+  wikiText = loadStrings('assets/text.txt');
+  font = loadFont('assets/fonts/SHPinscher-Regular.otf');
+  fontTitle = loadFont('assets/fonts/Chomsky.otf');
+  fontMain = loadFont('assets/fonts/SHPinscher-Regular.otf');
+
+  ///////////////////////////////////////////////   load images
+  for (let i = 0; i < numImages; i++) {
+    images[i] = loadImage('assets/images/img' + i + '.png');
+  }
+  soundFormats('mp3', 'wav');
+  //soundFile = loadSound('assets/audio/audio2.wav');
+  
+  ///////////////////////////////////////////////   load audio files
+  for (let i = 0; i < audioFilesCount; i++) {
+    audioFiles[i] = loadSound('assets/audio/audio' + i + '.wav');
+  }
+}
+
+function setup() {
+  ///////////////////////////////////////////////   WEBGL Camera set up
+  createCanvas(windowWidth, windowHeight,WEBGL);
+  cam = createCamera();
+  cam.ortho(-width/2, width/2, -height/2, height/2);
+  ///////////////////////////////////////////////   P5.Sound effect proccessing
+  delay = new p5.Delay();
+  reverb = new p5.Reverb();
+  dist = new p5.Distortion();
+  for (let i = 0; i<audioFilesCount;i++ ){ 
+    delay.process(audioFiles[i], 0.3, .8, 2300);
+    dist.process(audioFiles[i], distLevel);
+    reverb.process(audioFiles[i], 10, 10);
+  }
+  dist.chain(delay,reverb);
+  getAudioContext().suspend();
+  ///////////////////////////////////////////////   Setup
+  background(0);
+  setPoints();
+  frameRate(60);
+  ///////////////////////////////////////////////   File Input
+  //input = createFileInput(handleFile,true);
+  //input.position(width*0.5-45, 700);
+  //resizeCanvas(windowWidth, windowHeight);
+
+}
+
+function draw() {
+  background(0);  //sets background
+  cam.ortho(-width/2, width/2, -height/2, height/2);
+  //orbitControl(); /////////////////////////////   for debuging
+  chaoticAttr(); 
+  translate(-width*0.5,-height*0.5,0); //////////   moves drawing to top left (as its in WEBGL)
+  staticText();
+  movingText();
+  reverb.drywet(1);
+  visuals();
+
+  //text(mouseX +'/'+ mouseY, width*0.5,height*0.95)
+}
+
+function windowResized() {
+  resizeCanvas(windowWidth, windowHeight);
+  //input.position(width*0.5-45, 700);
+} 
+
+function mousePressed() {
+  userStartAudio(); //initalizes audio.
+  
+}
+
+function keyTyped() {
+  if (key === ' ') {
+    currentWikiText = wikiText[int(random(wikiText.length))]; //randomly chooses which line of the text file to use
+    splitString = split(currentWikiText, ' '); //splits the text file at each 'space' 
+    shuffle(splitString,true); //shuffles the text
+
+    audioFileSelect = int(random(audioFilesCount));
+
+    loopDuration = audioFiles[audioFileSelect].duration()/splitString.length;
+    //loopDuration = soundFile.duration()/splitString.length;
+    print('Loop Dur: '+loopDuration+' /audio' + audioFileSelect + '.wav');
+
+  } else {
+  }
+  
+}
+
+function keyPressed() {
+  if (keyCode === UP_ARROW) {
+    if (t<0.5){
+      t = t+0.005; 
+    }
+  } else if (keyCode === DOWN_ARROW) {
+    if (t>0.001){
+      t = t-0.005;
+    }
+  } else if (keyCode === RIGHT_ARROW) {
+    if (b<0.3){
+      b = b+0.005;
+    } 
+  } else if (keyCode === LEFT_ARROW) {
+    if (b>0.1){
+      b = b-0.005;
+    } 
+  }
+}
+
+function staticText(){
+  x = frameCount % width;
+  fill(255);
+  noStroke();
+  textSize(36);
+  textAlign(CENTER);
+  textFont(font);
+  if (getAudioContext().state == 'suspended'){
+    text('CLICK ANYWHERE TO START AUDIO.\n PRESS SPACE TO GENERATE A NEW TEXT.\nUSE ARROW KEYS TO CONTROL CHAOS.', width*0.5, height*0.5);
+  } else {
+    print('Audio: ON');
+  }
+
+  textFont(fontTitle);
+  text('OBSCURUS SONUS INSTRUMENTUM\nTENEBRARUM ATTRAHENS',width*0.5, 100) . //HEADER
+  fill(255);
+  textFont(font);
+  text(x,width*0.2,100) //number 
+
+  text(currentWikiText,width*0.5,700);  //plain text at bottom
+  text(round(t,3),width*0.25,height*0.6);
+  text(round(b,3),width*0.7,height*0.7);
+} 
+
+function movingText(){ ////////INCLUDES AUDIO PLAYBACK////////////
+  for (let i = 0; i<splitString.length;i++ ){
+    //Take coordinates from Chatoic Attractor and scale position from center to corner for mouse position sync
+    textFont(fontMain); //text setup
+    textSize(20);
+
+    let hoverXPos = xA[i]*(width/scale)+(width*0.5);
+    let hoverYPos = yA[i]*(width/scale)+(height*0.5);
+    let textBoxSize = 20; //hover over area of each word
+    if ((mouseX > hoverXPos-textBoxSize) && (mouseX < hoverXPos+textBoxSize) &&
+        (mouseY > hoverYPos-(textBoxSize*0.5)) && (mouseY < hoverYPos+(textBoxSize*0.5))) {
+      
+      fill(255, 0, 0);  //Sets text color to red if hovering over position
+      
+      ellipse(width*0.5, 100, 50, 50); ///////////// red circle in top left
+      vignetteColor = (255);
+
+      if (audioFiles[audioFileSelect].isPlaying()==false){
+        let sampleSpeed = hoverXPos/400;
+        distLevel = constrain(1-(yA[i]/2),0,1);
+        print(audioFileSelect)
+        audioFiles[audioFileSelect].play(0,sampleSpeed,1,i*loopDuration,loopDuration); 
+      }
+
+    } else {
+      fill(255);//Sets text color to white if NOT hovering over position
+      vignetteColor = (0);
+      //soundFile.stop();
+    }
+    
+    text(splitString[i].toUpperCase(),hoverXPos, hoverYPos);
+    textFont(font);
+    for (let y = 0; y < 5; y++){
+      text(splitString[int(random(y))], (width*0.1+(70*y)), 100+(i*10)); //text wall on left
+      text(splitString[i], (width*0.9), 700-(i*10)); //text wall on right
+      text(splitString[i], (width*0.85), 100+(i*12)); //text wall on right
+    }
+    text(splitString.length,width*0.8, height*0.8)
+    translate(width*0.5,height*0.5,0);
+
+    fill(vignetteColor);
+    sphere(1100-frameCount%200*t);
+    translate(-width*0.5,-height*0.5,0);
+    
+  }
+}
+
+function setPoints() { //////////////Sets up init position of particles texts//////////////
+  for (let i = 0; i < dim; i++) {
+    xA[i] = map(random(100),0,100,-1,1);
+    yA[i] = map(random(100),0,100,-1,1);
+    zA[i] = map(random(100),0,100,-1,1,true);
+  }
+}
+
+function chaoticAttr(){
+
+  /*
+  if (b < 0.23){
+    b = b + 0.0001;
+  } else {
+    b = 0.1;
+  }
+  */
+
+  // Thomas Attraktor - converted mathematical formula from http://www.3d-meier.de/tut19/Seite0.html
+  for (let i = 0; i < dim; i++) {
+
+    xM[i] = -b*xA[i]+sin(yA[i]);
+    yM[i] = -b*yA[i]+sin(zA[i]);
+    zM[i] = -b*zA[i]+sin(xA[i]);
+    
+    // Updating the position of the particles (and setting the speed)
+    xA[i] = xA[i]+(xM[i]*t);
+    yA[i] = yA[i]+(yM[i]*t);
+    zA[i] = zA[i]+(zM[i]*t);
+    
+    push(); 
+    for (let n = 0; n < dim; n++) {
+      xPos = xA[n]*(width/scale);
+      yPos = yA[n]*(width/scale);
+    }
+    pop();  
+  }
+}
+
+function visuals(){ //////////////VISUALS//////////////
+  if (frameCount %(60*(round(0.7-t,1))) == 0){ //////// events to occur every 60 frames multiplied by time (t) variable
+    cubeRotate++;
+    secRand = random(100);
+    shuffle(images, true); ///////// shuffles the images
+  }
+  for (let imgSel = 0; imgSel < numImages; imgSel++){
+    textureWrap(MIRROR);
+    push();
+    translate(width*0.2*imgSel,height*0.5,-1000);
+    rotateY(cubeRotate%(imgSel*100)*0.5*PI);
+    rotateX(cubeRotate*PI);
+    rotateZ(cubeRotate);
+    //let tex = images[int(random(imgSel))];
+    texture(images[imgSel]);
+    let imgScale = 0.8;
+    box(images[imgSel].width*imgScale,images[imgSel].height*imgScale,images[imgSel].width*imgScale)
+    pop();
+  }
+}
+
+
+/*
+function handleFile(file) { //////////////Loading Audio Files//////////////
+  if (file.type === 'audio') {
+    file = loadSound(file.data,'');
+    //file.hide();
+  } else if (file.type === 'image') {
+    //file = createImg(file.data, '');
+    //file.hide();
+  } else {
+    //file = null;
+  }
+}
+*/
