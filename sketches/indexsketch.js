861669e18cb5fce27c61567885ab5aa12fcbade1
Fauwrn <b.thorn0320211@arts.ac.uk>
2024-10-18 00:48:54 +0100

    revamp
    general redo

--- sketches/indexsketch.js
+++ sketches/indexsketch.js
@@ -2,7 +2,7 @@ let windowAspect;
 
 function preload() {
   ///////////////////////////////////////////////   load images
-  bgImg = loadImage('assets/sustained.png');
+  bgImg = loadImage('assets/background_images/Tree04S.jpg');
   portrait = loadImage('assets/Portrait1.png');
   gif = loadImage('assets/PlayerBW.gif');
 }
@@ -16,16 +16,29 @@ function setup() {
 
 function draw() {
   background(0);  //sets background
-  let mouseWRatio = 0.5-(mouseX/width);
-  let mouseHRatio = 0.5-(mouseY/height);
+  let mouseWRatio = 0.5+(mouseX/width);
+  let mouseHRatio = 0.5+(mouseY/height);
+
+  let paralaxAmt = 10;
+  push();
+  translate(-mouseWRatio*paralaxAmt,-mouseHRatio*paralaxAmt); ///PARALAXING
 
   imageMode(CENTER);
-  image(bgImg, width*0.5+(0.5-mouseWRatio*20), height*0.5+(0.5-mouseHRatio*20), 1920, 1080); //background image
+  image(bgImg, width*0.5, height*0.5, 1920, 1080); //background image
   
+  
+  pop();
   noStroke();
-  rectMode(CENTER);
-  let paralaxAmt = 10;
-  translate(mouseWRatio*paralaxAmt,mouseHRatio*paralaxAmt); ///PARALAXING
+  fill(0);
+  rect(0,0,width/4,height);
+  rect((width/4)*3,0,width/4,height);
+  rect(0,0,width,height/4);
+  rect(0,(height/4)*3,width,height);
+  
+  push();
+  imageMode(CENTER);
+  image(gif,width*0.5,height*0.5);
+  pop();
 }
 
 function windowResized() {
